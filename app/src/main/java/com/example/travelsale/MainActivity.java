acf77b503f24008e372db77e8287c461b25989e5
Che <profesor76@hotmail.com>
2022-05-26 12:45:52 -0500

    Uno

--- app/src/main/java/com/example/travelsale/MainActivity.java
+++ app/src/main/java/com/example/travelsale/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.travelsale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText ( getApplicationContext (), "Has seleccionado Paris", Toast.LENGTH_LONG ).show ();
+                Intent intent = new Intent ( MainActivity.this, ContentActivity.class );
+                startActivity ( intent );
             }
         } );
     }
