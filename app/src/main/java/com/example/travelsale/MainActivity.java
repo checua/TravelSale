7c57cb271c944d69c6628178ce83bcb3aef2ed2f
Che <profesor76@hotmail.com>
2022-05-30 12:27:13 -0500

    Menu

--- app/src/main/java/com/example/travelsale/MainActivity.java
+++ app/src/main/java/com/example/travelsale/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.travelsale;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -24,5 +28,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity ( intent );
             }
         } );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater ();
+        inflater.inflate ( R.menu.menu, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId ()){
+            case R.id.ajustes:
+                Toast.makeText ( getApplicationContext (), "Has pulsado Ajustes", Toast.LENGTH_LONG ).show ();
+                return true;
+            case R.id.ayuda:
+                Toast.makeText ( getApplicationContext (), "Has pulsado Ayuda", Toast.LENGTH_LONG ).show ();
+                return true;
+            case R.id.extra:
+                Toast.makeText ( getApplicationContext (), "Has pulsado Extra", Toast.LENGTH_LONG ).show ();
+                return true;
+            default:
+                return super.onOptionsItemSelected ( item );
+        }
+
     }
 }
